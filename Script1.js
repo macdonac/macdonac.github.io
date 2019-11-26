e09bfd8158213f51a17ed44fe31e6298c4b6c1c8
Bonnie ice <amar.m.suljic@gmail.com>
2019-11-25 22:39:59 -0500

    v

--- Script1.js
+++ Script1.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-fetch('aapi.openweathermap.org/data/2.5/weather?q=London')
+fetch('https://aapi.openweathermap.org/data/2.5/weather?q=London')
 	.then(response => response.json())
 	.then(data => {
 		console.log(data)
