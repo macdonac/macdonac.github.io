104f17d94be85b136fa113e149a758e2184594a8
Chris Macdonald <macdonac00@gmail.com>
2019-12-03 22:40:07 -0500

    styles

--- Script1.js
+++ Script1.js
@@ -8,13 +8,14 @@ $(document).ready(function () {
 	
 	
 function isValidUsZip(zip) {
-	console.log(Number.isInteger(parseInt(zip)));
+    console.log(Number.isInteger(parseInt(zip)));
+    return Number.isInteger(parseInt(zip))
 }
 
 
 
 function fetchWeather(zip) {
-	if (isValidUsZip(zip) == true) {
+	if (isValidUsZip(zip)) {
 	 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zip},us&units=imperial&APPID=f9940e48694b3fdded1e8d7c56ca3c17`)
         .then(response => response.json())
         .then(data => {            
